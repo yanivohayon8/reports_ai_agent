377d16c64333b99a05f4a29085277a63bf4f290b
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-07-21 18:04:55 +0300

    comments

--- src/tools/question_answer.py
+++ src/tools/question_answer.py
@@ -6,9 +6,11 @@ class QuestionAnswer:
     def __init__(self,text:str):
         self.chat_model = get_openai_model()
 
+        self.transformed_text = self._transform_to_qa(text)
 
 
     def _transform_to_qa(self,text:str,num_questions:int=10)->str:
+        # TODO: optimize this
         system_message= """You are an expert of writing questions given a text. 
                             Given the following text, write {num_questions} questions and their answers.
                             Use the following format:
