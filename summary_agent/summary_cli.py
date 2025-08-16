d9eecf61cd74bd28bd7b6ee930c20f4fd99f4b3a
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-08-16 15:25:31 +0300

    summary cli

--- summary_agent/summary_cli.py
+++ summary_agent/summary_cli.py
@@ -1,18 +1,21 @@
 import argparse
+import sys
+from  pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
 
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from core.api_utils import get_llm_langchain_openai
 from summary_agent.summary import SummaryAgent
-from  pathlib import Path
+from core.text_splitter import get_text_splitter    
 
 def main():
     parser = argparse.ArgumentParser(description="Summarize a PDF file")
     parser.add_argument("pdf_path", type=str, help="The path to the PDF file")
-    parser.add_argument("method", type=str, help="The method to use for summarization")
+    parser.add_argument("--method", type=str, help="The method to use for summarization")
+    parser.add_argument("--model", type=str, help="The model to use for summarization", default="gpt-4o-mini")
     args = parser.parse_args()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300,chunk_overlap=50)
-    llm = get_llm_langchain_openai(model="gpt-4o-mini")
+    text_splitter = get_text_splitter()
+    llm = get_llm_langchain_openai(model=args.model)
     summary_agent = SummaryAgent(text_splitter,llm)
     summary = summary_agent.summarize_single_pdf(Path(args.pdf_path), args.method)
     print(summary)
