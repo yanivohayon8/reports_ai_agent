9b71b07e4b5a47dad62a939703195d9116d7caeb
Yaniv Ohayon <yanivohayon8@gmail.com>
2025-08-21 16:08:48 +0300

    create_dataset_from_text

--- agents/needle_agent/evaluator.py
+++ agents/needle_agent/evaluator.py
@@ -2,40 +2,54 @@ from core.pdf_reader import read_pdf
 from core.text_splitter import get_text_splitter
 import random
 from langchain_core.language_models import BaseChatModel
-from core.api_utils import get_llm_langchain_openai
-
 from agents.needle_agent.needle_prompts import evaluation_Qna_template
 import json
 
 class DatasetCreator:
-    def __init__(self, llm:BaseChatModel):
+    def __init__(self, llm:BaseChatModel, 
+                 min_chunk_size: int=400, max_chunk_size: int=1000,
+                 min_chunk_overlap: int=100, max_chunk_overlap: int=200):
         self.llm = llm
-    
-    @classmethod
-    def from_gpt(cls, model:str="gpt-4o-mini",**kwargs):
-        llm = get_llm_langchain_openai(model=model,**kwargs)
-        
-        return cls(llm)
+        self.min_chunk_size = min_chunk_size
+        self.max_chunk_size = max_chunk_size
+        self.min_chunk_overlap = min_chunk_overlap
+        self.max_chunk_overlap = max_chunk_overlap
 
     def create_dataset(self):
         pass
 
-    def create_dataset_from_pdf(self, pdf_path: str):
-        pass
+    def create_dataset_from_text(self, text: str, n_chunks: int=10):
+        chunks = self._create_chunks(text)
+        random_chunks = self._select_random_chunk(chunks, n_chunks=n_chunks)
+        questions_and_answers = self._generate_questions_and_answers(random_chunks)
 
-    def _create_random_chunk(self,text: str, chunk_size: int, chunk_overlap: int):
+        return questions_and_answers
+
+    def _create_chunks(self, text: str):
+        chunk_size = random.randint(self.min_chunk_size, self.max_chunk_size)
+        chunk_overlap = random.randint(self.min_chunk_overlap, self.max_chunk_overlap)
+        chunks = self._create_chunks_by_size(text, chunk_size, chunk_overlap)
+        return chunks
+
+    def _create_chunks_by_size(self,text: str, chunk_size: int, chunk_overlap: int):
         text_splitter = get_text_splitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
         chunks = text_splitter.split_text(text)
-        random_chunk = random.choice(chunks)
-        
-        return random_chunk
+        return chunks
     
+    def _select_random_chunk(self,chunks: list[str], n_chunks: int)->list[str]:
+        random_chunks = random.sample(chunks, n_chunks)
+        return random_chunks
+    
+    def _generate_questions_and_answers(self,chunks: list[str])->list[dict]:
+        questions_and_answers = [self._generate_answer_and_question(chunk) for chunk in chunks]
+        return questions_and_answers
+
     def _generate_answer_and_question(self,chunk: str)->dict:
         prompt = evaluation_Qna_template.invoke({"chunk":chunk})
         response = self.llm.invoke(prompt)
-        response_json = json.loads(response.content)
+        question_and_answer = json.loads(response.content)
 
-        return response_json
+        return question_and_answer
 
 
     
--- tests/test_needle_evaluator.py
+++ tests/test_needle_evaluator.py
@@ -2,19 +2,44 @@ import os
 from agents.needle_agent.evaluator import DatasetCreator
 from core.pdf_reader import read_pdf
 from pathlib import Path
+from core.api_utils import get_llm_langchain_openai
+
 
 def test_dataset_create_chunk():
-    dataset_creator = DatasetCreator.from_gpt()    
+    llm = get_llm_langchain_openai(model="gpt-4o-mini")
+    dataset_creator = DatasetCreator(llm)    
     pdf_path = Path(os.path.join("tests","data","report.pdf"))
     text = read_pdf(pdf_path, format="text")
     chunk_size = 400
     chunk_overlap = 100
-    chunk = dataset_creator._create_random_chunk(text, chunk_size=chunk_size,chunk_overlap=chunk_overlap)
-    assert isinstance(chunk, str)
+    chunks = dataset_creator._create_chunks_by_size(text, 
+                                                   chunk_size=chunk_size,
+                                                   chunk_overlap=chunk_overlap)
+    assert isinstance(chunks, list)
+    assert len(chunks) > 0
+    assert isinstance(chunks[0], str)
 
-    response_json = dataset_creator._generate_answer_and_question(chunk)
+    response_json = dataset_creator._generate_answer_and_question(chunks[0])
     assert isinstance(response_json, dict)
     assert "question" in response_json
     assert "answer" in response_json
+
+def test_create_chunks():
+    llm = get_llm_langchain_openai(model="gpt-4o-mini")
+    dataset_creator = DatasetCreator(llm)    
+    pdf_path = Path(os.path.join("tests","data","report.pdf"))
+    text = read_pdf(pdf_path, format="text")
+
+    questions_and_answers = dataset_creator.create_dataset_from_text(text, n_chunks=2)
+    assert isinstance(questions_and_answers, list)
+    assert len(questions_and_answers) > 0
+    assert isinstance(questions_and_answers[0], dict)
+    assert "question" in questions_and_answers[0]
+    assert "answer" in questions_and_answers[0]
+
+
+
+
+
 
 
